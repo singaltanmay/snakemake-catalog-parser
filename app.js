3e98fb41893e45b992ceddea8c8fd0378c06b288
Tanmay Singal <tanmaysingal@icloud.com>
2023-07-31 23:18:17 +0530

    changes

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const fs = require('fs');
 
-function createToolVersionRegisterId() {
+function createToolVersionRegisterId(
+    id,
+    author,
+    descriptor_type,
+    files,
+    images,
+    included_apps,
+    is_production,
+    name,
+    signed,
+    verified,
+    verified_source
+) {
     return {
         id,
         author,
@@ -41,7 +53,10 @@ function createToolRegister(description, aliases, checkerUrl, hasChecker, name, 
 // Convert a SWC object into a TRS-Filer POST API object
 function trsConverter(swcObj) {
     console.log(swcObj);
-    return createToolRegister(swcObj.description, null, null, null, swcObj.full_name, null, null, null)
+    console.log("---------------")
+    const version = createToolVersionRegisterId(0, [swcObj.full_name], "CWL", null, null, null, true, swcObj.full_name, false, true, ["Snakemake Workflow Catalog"]);
+    const toolclass = createToolclassRegisterId(swcObj.description, null, swcObj.full_name);
+    return createToolRegister(swcObj.description, null, null, null, swcObj.full_name, "https://www.github.com/" + swcObj.full_name.split('/')[0], toolclass, [version])
 }
 
 const filePath = "/home/tanmay/Documents/snakemake-catalog-parser/data.js";
@@ -50,5 +65,5 @@ const filePath = "/home/tanmay/Documents/snakemake-catalog-parser/data.js";
 fs.readFile(filePath, function (error, content) {
     //console.log(content.byteOffset(1000))
     const data = JSON.parse(content);
-    console.log(trsConverter(data[0]));
+    console.log(JSON.stringify(trsConverter(data[0])));
 });
